fd098a6471d78fce7fc3110fe1322b65c55beb0e
J-push <90913626+J-push@users.noreply.github.com>
2022-05-02 12:47:25 +0900

    Add files via upload

---
+++ 문제풀이(시험)/StackArray.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct stack // ±¸±Û¸µ
+{
+	int arr[10000];
+	int last;
+
+	void init()
+	{
+		last = -1;
+	}
+	void push(int data)
+	{
+		arr[++last] = data;
+	}
+	bool empty()
+	{
+		return (last < 0);
+	}
+	int pop()
+	{
+		if (empty()) 
+		{
+			return -1;
+		}
+		return arr[last--];
+	}
+	int size()
+	{
+		return last + 1;
+	}
+	int top()
+	{
+		if (empty())
+		{
+			return -1;
+		}
+		return arr[last];
+	}
+};
+
+int main() 
+{
+	int n;
+	cin >> n;
+	stack s;
+	s.init();
+	for (; n--;) 
+	{
+		string str;
+		int num;
+		cin >> str;
+		if (str == "push") 
+		{
+			cin >> num;
+			s.push(num);
+		}
+		if (str == "pop") 
+		{
+			cout << s.pop() << "\n";
+		}
+		if (str == "size") 
+		{
+			cout << s.size() << "\n";
+		}
+		if (str == "empty") 
+		{
+			cout << s.empty() << "\n";
+		}
+		if (str == "top") 
+		{
+			cout << s.top() << "\n";
+		}
+	}
+}
---
+++ 문제풀이(시험)/문제풀이(시험).cpp
@@ -0,0 +1,60 @@
+﻿// 문제풀이(시험).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// 피보나치
+int Pibonacci(int a)
+{	
+	if (a == 1)
+	{
+		return 1;
+	}
+	else if (a == 2)
+	{
+		return 1;
+	}
+	else
+	{
+		//세번째 수부턴
+		return Pibonacci(a - 1) + Pibonacci(a - 2);
+	}
+}
+
+// 하노이탑(구글링)
+void Hanoi(int n, int from, int by, int to) // 원판 개수, 출발지, 경유지, 목적지
+{
+	if (n == 1)
+	{
+		cout << from << " " << to << endl;
+	}
+	else
+	{
+		Hanoi(n - 1, from, to, by);
+		cout << from << " " << to << endl;
+		Hanoi(n - 1, by, from, to);
+	}
+}
+
+int main()
+{
+	int num = 0;
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		cout << Pibonacci(i) << " ";
+	}
+
+	cout << endl;
+
+	int n;
+	cin >> n;
+	int k = pow(2, n) - 1;
+	cout << k << endl;
+	Hanoi(n, 1, 2, 3);
+	return 0;
+}
+
+
